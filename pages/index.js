6548d24a6b083ba1acd735be80f8b0ec1bf854cc
emirodriguezalc <70884463+emirodriguezalc@users.noreply.github.com>
2021-04-30 19:17:13 +0200

    code refactor

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import { useState } from 'react';
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { useState, useEffect } from 'react';
 import Product from '../components/Products/Products';
 import Footer from '../components/Footer/Footer';
 import {
@@ -12,13 +11,61 @@ import {
   IconButton,
   useToast
 } from "@chakra-ui/react";
-import {SearchIcon, CloseIcon} from "@chakra-ui/icons"
+import { SearchIcon, CloseIcon } from "@chakra-ui/icons"
 
-export default function Home(results) {
-  const initialState = results;
-  const [products, setProducts] = useState(initialState.products);
+export default function Home() {
+  const [products, setProducts] = useState("");
   const [search, setSearch] = useState("");
+  const [actualPage, setActualPage] = useState(1);
   const toast = useToast();
+
+  useEffect(() => {
+    suggestedProducts();
+  }, [])
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const results = await fetch("/api/SearchProducts", {
+      method: "post",
+      body: search,
+    });
+    const { products, error } = await results.json();
+    if (error) {
+      toast({
+        position: "bottom",
+        title: "An error ocurred",
+        description: error,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      setProducts(products);
+    }
+  }
+
+  const suggestedProducts = async () => {
+    const results = await fetch("/api/GetProducts", {
+      method: "post",
+      body: actualPage,
+    });
+    const { products, error } = await results.json();
+    if (error) {
+      toast({
+        position: "bottom",
+        title: "An error ocurred",
+        description: error,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      setProducts(products);
+    }
+  }
+
+
+
   return (
     <Flex direction="column" justify="center" align="center">
       <Head>
@@ -29,89 +76,30 @@ export default function Home(results) {
         <Heading as="h1" size="2xl" mb={8}>
           No covid shopping spot
           </Heading>
-          <form onSubmit={async (e) => {
-            e.preventDefault();
-            const results = await fetch("/api/SearchProducts",{
-              method: "post",
-              body: search,
-            });
-            const { products, error} = await results.json();
-            if(error) {
-              toast({
-                position: "bottom",
-                title: "An error ocurred",
-                description: error, 
-                status: "error", 
-                duration: 5000, 
-                isClosable: true,
-              });
-            } else {
-              setProducts(products);
-            }
-          }}>
-            <Stack maxWidth="350px" width="100%" isInline mb={8}>
-              <Input placeholder="Search for a product" value={search} border="none" onChange={(e) => setSearch(e.target.value)}>
-              </Input>
-              <IconButton 
-              colorScheme="blue" 
+        <form onSubmit={handleSearch}>
+          <Stack maxWidth="350px" width="100%" isInline mb={8}>
+            <Input placeholder="Search for a product" value={search} border="none" onChange={(e) => setSearch(e.target.value)}>
+            </Input>
+            <IconButton
+              colorScheme="blue"
               aria-label="Search Database"
               icon={<SearchIcon />}
               disabled={search === ""}
               type="submit" />
 
-              <IconButton colorScheme="red"
+            <IconButton colorScheme="red"
               aria-label="Reset Button"
               icon={<CloseIcon />}
               disabled={search === ""}
               onClick={async () => {
                 setSearch("");
-                setProducts(initialState.products);
-              }}/>
-              
-            </Stack>
-          </form>
-        <Product products={products} />
+                suggestedProducts();
+              }} />
+          </Stack>
+        </form>
+        {products && <Product products={products} />}
       </Box>
       <Footer />
     </Flex>
   )
-}
-
-export async function getStaticProps() {
-  const client = new ApolloClient({
-    uri: "https://rickandmortyapi.com/graphql/",
-    cache: new InMemoryCache(),
-  });
-  const { data } = await client.query({
-    query: gql`
-    query {
-      characters(page: 1) {
-        info {
-          count
-          pages
-        }
-        results {
-          name
-          id
-          location {
-            name
-            id
-          }
-          image
-          episode {
-            id
-            episode
-            air_date
-          }
-        }
-      }
-    }
-    `,
-  });
-
-  return {
-    props: {
-      products: data.characters.results,
-    },
-  }
 }
